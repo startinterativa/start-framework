52397e1485a726412b1118811767e72a953e115d
Vitor Marcelino <vitormarcelino93@gmail.com>
2018-09-02 10:03:35 -0300

    Change diff method

--- Classes/Support/Diff.php
+++ Classes/Support/Diff.php
@@ -6,16 +6,17 @@
         public function logDiff($before, $after) {
             $arrayBefore = get_object_vars($before);
             $arrayAfter = get_object_vars($after);
-            
-            $diff = array_diff($arrayBefore, $arrayAfter);
+
             $log = '';
             
-            foreach ($diff as $attr => $value) {
-                if ($attr == 'data') {
-                    $value = date('d/m/Y H:i', (int)$value);
-                    $arrayAfter[$attr] = date('d/m/Y H:i', (int)$arrayAfter[$attr]);
+            foreach ($arrayBefore as $attr => $value) {
+                if($arrayAfter[$attr] != $value) {
+                    if ($attr == 'data') {
+                        $value = date('d/m/Y H:i', (int)$value);
+                        $arrayAfter[$attr] = date('d/m/Y H:i', (int)$arrayAfter[$attr]);
+                    }
+                    $log .= "Alterou ".$attr. ": " . $value . " -> " . $arrayAfter[$attr].". ";
                 }
-                $log .= "Alterou ".$attr. ": " . $value . " -> " . $arrayAfter[$attr].". ";
             }
             
             if(empty($log)) {
